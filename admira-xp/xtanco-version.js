df86b78bb96a4c1e449cd34e4385f62937595721
csilvasantin <csilvasantin@MacMini.local>
2026-06-14 12:23:24 +0200

    admira-xp: sync gemelo 20260614-0002 (caja Metahuman arrastrable + /avatar3d on totem)

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.14.1',
-    build: '20260614-0001',
-    cacheName: 'admiranext-v26-06-14-0001',
+    version: 'AdmiraNext v26.06.14.2',
+    build: '20260614-0002',
+    cacheName: 'admiranext-v26-06-14-0002',
   });
 })();
