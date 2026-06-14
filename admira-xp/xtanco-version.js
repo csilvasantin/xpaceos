b9485baad3e01e9d67e16e714da547bd720b8ffe
csilvasantin <csilvasantin@MacMini.local>
2026-06-14 13:22:07 +0200

    admira-xp: sync 20260614-0006 (micro de voz en el cuadro Metahuman)

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.14.5',
-    build: '20260614-0005',
-    cacheName: 'admiranext-v26-06-14-0005',
+    version: 'AdmiraNext v26.06.14.6',
+    build: '20260614-0006',
+    cacheName: 'admiranext-v26-06-14-0006',
   });
 })();
