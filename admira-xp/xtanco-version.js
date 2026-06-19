96430eb53f09ab9b1599d6d10ee027b89fe7616f
Carlos Silva <csilva@admira.com>
2026-06-19 13:30:16 +0200

    sync(admira-xp): halo permanente + separacion NPCs + v20260619-0004

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.19.3',
-    build: '20260619-0003',
-    cacheName: 'admiranext-v26-06-19-0003',
+    version: 'AdmiraNext v26.06.19.4',
+    build: '20260619-0004',
+    cacheName: 'admiranext-v26-06-19-0004',
   });
 })();
