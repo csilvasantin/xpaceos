90740f860895925b899219095e09299550c9312d
Carlos Silva <csilva@admira.com>
2026-06-19 14:05:16 +0200

    sync(admira-xp): ficha NPC con audiencia de pixeria + v20260619-0006

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.19.5',
-    build: '20260619-0005',
-    cacheName: 'admiranext-v26-06-19-0005',
+    version: 'AdmiraNext v26.06.19.6',
+    build: '20260619-0006',
+    cacheName: 'admiranext-v26-06-19-0006',
   });
 })();
