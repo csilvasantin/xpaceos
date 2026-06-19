62cefd24dc61303805810a790dd6128323a3b6f3
Carlos Silva <csilva@admira.com>
2026-06-19 13:05:47 +0200

    sync(admira-xp): entrada llamativa del NPC al llegar a la tienda + v20260619-0002

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.19.1',
-    build: '20260619-0001',
-    cacheName: 'admiranext-v26-06-19-0001',
+    version: 'AdmiraNext v26.06.19.2',
+    build: '20260619-0002',
+    cacheName: 'admiranext-v26-06-19-0002',
   });
 })();
