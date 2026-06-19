7e628f810ce59191a83d16bad0bf205645108dfc
Carlos Silva <csilva@admira.com>
2026-06-19 12:15:50 +0200

    sync(admira-xp): espejo regenerado desde game.html (NPC Anonimizador a escala + marca ¡!) + v20260619-0001

--- admira-xp/xtanco-version.js
+++ admira-xp/xtanco-version.js
@@ -4,8 +4,8 @@
   // YY is the year, DD.MM is the current day/month, R restarts at 1 each day.
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v26.06.17.8',
-    build: '20260617-0008',
-    cacheName: 'admiranext-v26-06-17-0008',
+    version: 'AdmiraNext v26.06.19.1',
+    build: '20260619-0001',
+    cacheName: 'admiranext-v26-06-19-0001',
   });
 })();
